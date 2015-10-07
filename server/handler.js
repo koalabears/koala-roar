19f181c4a964b402d302074c41db5baa57089e66
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2015-10-07 14:36:45 +0100

    basic server functionality

---
+++ server/handler.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+
+module.exports = function(req, res) {
+  var index = fs.readFileSync(__dirname + '/../public/html/index.html');
+  var indexStyle = fs.readFileSync(__dirname + '/../public/css/main.css');
+  var indexJS = fs.readFileSync(__dirname + '/../public/js/main.js');
+
+  var url = req.url;
+  if (url === '/') {
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.end(index);
+  } else if (url === '/main.css') {
+    res.writeHead(200, {'Content-Type' : 'text/css'});
+    res.end(indexStyle);
+  } else if (url === '/main.js') {
+    res.writeHead(200, {'Content-Type' : 'text/js'});
+    res.end(indexJS);
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+}
