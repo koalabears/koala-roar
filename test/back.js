1502073f41f334f8e43e9fe55b0af041e13af76c
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2015-10-07 14:35:36 +0100

    added dynamic server get response testing

---
+++ test/back.js
@@ -0,0 +1,35 @@
+var test = require('tape');
+var shot = require('shot');
+var handler = require('../server/handler.js');
+
+console.log(typeof handler);
+
+test("handler is function", function(t) {
+  t.equal(typeof handler, 'function');
+  t.end();
+});
+
+function testGetResponse(url, statusCode) {
+  var str = 'checking handler responds with status code ';
+  test(str + statusCode + ' for url ' + url, function(t) {
+    var req = {
+      method: 'GET',
+      url: url
+    };
+    shot.inject(handler, req, function(res) {
+      t.equal(res.statusCode, statusCode);
+      t.end();
+    });
+  });
+}
+
+var getTests = [
+    {statusCode: 404, url: '/randomy/'},
+    {statusCode: 200, url: '/'},
+    {statusCode: 200, url: '/main.js'},
+    {statusCode: 200, url: '/main.css'}
+];
+
+getTests.forEach(function(testCase) {
+  testGetResponse(testCase.url, testCase.statusCode)
+});
