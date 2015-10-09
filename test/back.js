187e557553cd114769add4979dfbd2ef828f4fc3
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2015-10-09 13:03:49 +0100

    fixed references to handler causing everything to crash

--- test/back.js
+++ test/back.js
@@ -1,14 +1,7 @@
 var test = require('tape');
 var shot = require('shot');
-var server = require('../server/handler.js');
+var handler = require('../server/handler.js');
 var db = require('../server/redis.js');
-
-console.log(typeof handler);
-
-test("handler is function", function(t) {
-  t.equal(typeof server.handler, 'function', 'the handler is a function');
-  t.end();
-});
 
 function testGetResponse(url, statusCode) {
   var str = 'checking handler responds with status code ';
@@ -17,7 +10,7 @@ function testGetResponse(url, statusCode) {
       method: 'GET',
       url: url
     };
-    shot.inject(server.handler, req, function(res) {
+    shot.inject(handler, req, function(res) {
       t.equal(res.statusCode, statusCode, 'Status code for response and the actual status code matches!');
       console.log(res.statusCode);
       t.end();
