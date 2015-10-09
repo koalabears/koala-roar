bfc823d49e373d0834db2a3e45abe42d1e1eb473
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2015-10-09 12:32:18 +0100

    removing IIFE

--- server/handler.js
+++ server/handler.js
@@ -1,60 +1,69 @@
 var fs = require('fs');
 
-var server = (function() {
 
-  var db = require('./redis.js');
-  var index = fs.readFileSync(__dirname + '/../public/html/index.html');
-  var indexStyle = fs.readFileSync(__dirname + '/../public/css/main.css');
-  var indexJS = fs.readFileSync(__dirname + '/../public/js/main.js');
-  var cookieJS = fs.readFileSync(__dirname + '/../public/js/cookie.js');
+var db = require('./redis.js');
+var index = fs.readFileSync(__dirname + '/../public/html/index.html');
+var indexStyle = fs.readFileSync(__dirname + '/../public/css/main.css');
+var indexJS = fs.readFileSync(__dirname + '/../public/js/main.js');
+var cookieJS = fs.readFileSync(__dirname + '/../public/js/cookie.js');
 
-  function handler(req, res) {
-    var url = req.url;
-    console.log(url);
-    if (url === '/') {
-      res.writeHead(200, {'Content-Type' : 'text/html'});
-      res.end(index);
-    } else if (url === '/main.css') {
-      res.writeHead(200, {'Content-Type' : 'text/css'});
-      res.end(indexStyle);
-    } else if (url === '/main.js') {
-      res.writeHead(200, {'Content-Type' : 'text/js'});
-      res.end(indexJS);
-    } else if (url === '/cookie.js') {
-        res.writeHead(200, {'Content-Type' : 'text/js'});
-        res.end(cookieJS);
-    } else if (url.match(/^(\/test)/)) {
-      serveTest(req, res);
-    } else if (url.match(/^(\/roars)/)) {
-      db.postRoar(req, res);
-    } else if (url === '/allPosts') {
-      db.printPosts(req, res);
-    } else if(url.match(/^(\/users)/)) {
-      db.createUserId(req,res);
-    } else {
-      res.writeHead(404);
-      res.end();
-    }
+function handler(req, res) {
+  var url = req.url;
+  console.log(url);
+  if (url === '/') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    res.end(index);
+  } else if (url === '/main.css') {
+    res.writeHead(200, {
+      'Content-Type': 'text/css'
+    });
+    res.end(indexStyle);
+  } else if (url === '/main.js') {
+    res.writeHead(200, {
+      'Content-Type': 'text/js'
+    });
+    res.end(indexJS);
+  } else if (url === '/cookie.js') {
+    res.writeHead(200, {
+      'Content-Type': 'text/js'
+    });
+    res.end(cookieJS);
+  } else if (url.match(/^(\/test)/)) {
+    serveTest(req, res);
+  } else if (url.match(/^(\/roars)/)) {
+    db.postRoar(req, res);
+  } else if (url === '/allPosts') {
+    db.printPosts(req, res);
+  } else if (url.match(/^(\/users)/)) {
+    db.createUserId(req, res);
+  } else {
+    res.writeHead(404);
+    res.end();
   }
+}
 
-  function serveTest(req, res){
-    var test = fs.readFileSync(__dirname + '/../test/front-end/test.html');
-    var testjs = fs.readFileSync(__dirname + '/../test/front-end/test.js');
-    if (req.url === '/test.html'){
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(test.toString());
-    } else if (req.url === '/test.js') {
-      res.writeHead(200, {'Content-Type': 'text/javascript'});
-      res.end(testjs.toString());
-    } else {
-      res.writeHead(404, {'Content-Type': 'text/javascript'});
-      res.end('error: ' + req.url + ' not found');
-    }
+function serveTest(req, res) {
+  var test = fs.readFileSync(__dirname + '/../test/front-end/test.html');
+  var testjs = fs.readFileSync(__dirname + '/../test/front-end/test.js');
+  if (req.url === '/test.html') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    res.end(test.toString());
+  } else if (req.url === '/test.js') {
+    res.writeHead(200, {
+      'Content-Type': 'text/javascript'
+    });
+    res.end(testjs.toString());
+  } else {
+    res.writeHead(404, {
+      'Content-Type': 'text/javascript'
+    });
+    res.end('error: ' + req.url + ' not found');
   }
+}
 
-  return {
-    handler: handler,
-  };
-}());
 
-module.exports = server;
+module.exports = handler;
